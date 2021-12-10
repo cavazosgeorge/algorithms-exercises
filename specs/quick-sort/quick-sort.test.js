52cee979b661f238ff78f8a77c78343225e39e82
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-09 23:13:24 -0500

    solved quicksort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -13,12 +13,34 @@
 */
 
 function quickSort(nums) {
-  // code goes here
+  // base case, array of length 0 or 1
+  if (nums.length <= 1) return nums;
+
+  // choose a pivot
+  const pivot = nums[nums.length - 1];
+
+  // seperaite into left and right ararys
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < pivot) {
+      left.push(nums[i]);
+    } else {
+      right.push(nums[i]);
+    }
+  }
+  // call quicksort on left and right arrays
+  const sortedLeft = quickSort(left);
+  const sortedRight = quickSort(right);
+
+  // return left.concat(pivot, right)
+  return sortedLeft.concat(pivot, sortedRight);
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
